511c450a2d590b4657f6d34c93fd24fd7bac6aa4
hongjunxiao <1173937600@qq.com>
2016-11-07 17:25:15 +0800

    init

---
+++ WeChat_OC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WeChat_OC
+//
+//  Created by xiaohongjun on 2016/11/7.
+//  Copyright © 2016年 xiaohongjun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WeChat_OC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WeChat_OC
+//
+//  Created by xiaohongjun on 2016/11/7.
+//  Copyright © 2016年 xiaohongjun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
